0589b9b0167a5e92f28b23ffb1462479d4cb69d6
Tuan Trung <tranvantuantrung@gmail.com>
2020-04-10 20:24:44 +0700

    use pug template

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views', './views')
+
+
 app.get('/', function(req, res) {
-    res.send('hello, codersX');
+    res.render('say-hello/index.pug', {
+        name: "codersX"
+    });
 });
 
 app.get('/name', function(req, res) {
-    res.send('my name is Trung');
+    res.render('my-name/index.pug');
 });
 
-app.get('/age', function(req, res) {
-    res.send('19');
+app.get('/users', function(req, res) {
+    res.render('users/index.pug', {
+        users: [
+            'trung', 'hieu', 'thien'
+        ]
+    });
 });
 
 app.listen(port, function() {
