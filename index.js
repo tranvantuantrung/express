1fce1a50248810edc2d6c43f9911b5b7e6d9843d
Tuan Trung <tranvantuantrung@gmail.com>
2020-04-11 19:41:34 +0700

    re-settup

--- index.js
+++ index.js
@@ -10,63 +10,62 @@ const adapter = new FileSync('db.json');
 const db = low(adapter);
 
 // Set some defaults (required if your JSON file is empty)
-db.defaults({ users: [] })
-  .write()
-
-
+db.defaults({ users: [] }).write();
 app.set('view engine', 'pug');
-app.set('views', './views')
+app.set('views', './views');
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get('/', function(req, res) {
-    res.render('say-hello/index.pug', {
-        name: "codersX"
+app.get('/', function (req, res) {
+  res.render('index.pug', {
+    name: 'codersX',
+  });
+});
+
+app.get('/name', function (req, res) {
+  res.render('my-name.pug');
+});
+
+app.get('/users', function (req, res) {
+  res.render('users/index.pug', {
+    users: db.get('users').value(),
+  });
+});
+
+app.get('/users/search', function (req, res) {
+  let nameSearch = req.query.name;
+  let nameFilter = db
+    .get('users')
+    .value()
+    .filter(function (user) {
+      return user.name.toLowerCase().indexOf(nameSearch.toLowerCase()) !== -1;
     });
+
+  res.render('users/index.pug', {
+    users: nameFilter,
+    nameSearch: nameSearch,
+  });
 });
 
-app.get('/name', function(req, res) {
-    res.render('my-name/index.pug');
+app.get('/users/create', function (req, res) {
+  res.render('users/create.pug');
 });
 
-app.get('/users', function(req, res) {
-    res.render('users/index.pug', {
-        users: db.get('users').value()
-    });
+app.post('/users/create', function (req, res) {
+  req.body.id = shortid.generate();
+  db.get('users').push(req.body).write();
+  res.redirect('/users');
 });
 
-app.get('/users/search', function(req, res) {
-    let nameSearch = req.query.name;
-    let nameFilter = db.get('users').value().filter(function(user) {
-        return user.name.toLowerCase().indexOf(nameSearch.toLowerCase()) !== -1;
-    });
-    
-    res.render('users/index.pug', {
-        users: nameFilter,
-        nameSearch: nameSearch
-    })
+app.get('/users/:id', function (req, res) {
+  let id = req.params.id;
+  let user = db.get('users').find({ id: id }).value();
+  res.render('users/view', {
+    user: user,
+  });
 });
 
-app.get('/users/create', function(req, res) {
-    res.render('users/create/index.pug');
+app.listen(port, function () {
+  console.log('server listening on port ' + port);
 });
-
-app.post('/users/create', function(req, res) {
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users');
-});
-
-app.get('/users/:id', function(req, res) {
-    let id = req.params.id;
-    let user = db.get('users').find({ id: id}).value();
-    res.render('users/view', {
-        user: user
-    });
-});
-
-app.listen(port, function() {
-    console.log('server listening on port ' + port);
-});
-
