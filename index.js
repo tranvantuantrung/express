6891f3accc129a3fce4d57dc500210290f0ff984
Tuan Trung <tranvantuantrung@gmail.com>
2020-04-11 12:13:48 +0700

    use lowdb to save database

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [] })
+  .write()
+
 
 app.set('view engine', 'pug');
 app.set('views', './views')
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-let users = ['Trung', 'Hieu', 'Thien'];
 
 app.get('/', function(req, res) {
     res.render('say-hello/index.pug', {
@@ -23,14 +31,14 @@ app.get('/name', function(req, res) {
 
 app.get('/users', function(req, res) {
     res.render('users/index.pug', {
-        users: users
+        users: db.get('users').value()
     });
 });
 
 app.get('/users/search', function(req, res) {
     let nameSearch = req.query.name;
-    let nameFilter = users.filter(function(name) {
-        return name.toLowerCase().indexOf(nameSearch.toLowerCase()) !== -1;
+    let nameFilter = db.get('users').value().filter(function(user) {
+        return user.name.toLowerCase().indexOf(nameSearch.toLowerCase()) !== -1;
     });
     
     res.render('users/index.pug', {
@@ -44,7 +52,7 @@ app.get('/users/create', function(req, res) {
 });
 
 app.post('/users/create', function(req, res) {
-    users.push(req.body.name);
+    db.get('users').push(req.body).write();
     res.redirect('/users');
 });
 
