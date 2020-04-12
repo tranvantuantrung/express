1b11e821ae29def068aff9d7b7506290093bd56a
Tuan Trung <tranvantuantrung@gmail.com>
2020-04-12 22:38:00 +0700

    install package cookie

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const userRoute = require('./routes/users.route');
 
@@ -11,6 +12,7 @@ app.set('views', './views');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
 
 app.use(express.static('public'));
 
