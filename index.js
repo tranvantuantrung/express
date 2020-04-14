2e4261857864c17865eeb5cd5bc36e699941528b
Tuan Trung <tranvantuantrung@gmail.com>
2020-04-14 11:43:30 +0700

    add env variable by dotenv for cookie secret

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const secrectCookie = require('shortid');
 
 const userRoute = require('./routes/users.route');
 const authRoute = require('./routes/auth.route');
@@ -16,7 +17,7 @@ app.set('views', './views');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser(secrectCookie.generate()));
+app.use(cookieParser(process.env.SESSION_SECRET));
 
 app.use(express.static('public'));
 
