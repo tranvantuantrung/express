948851efe0cbeb8acf2f72f708467256ed4671b1
Tuan Trung <tranvantuantrung@gmail.com>
2020-04-15 11:46:45 +0700

    fix navbar for products

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -8,6 +8,13 @@ module.exports.index = function (req, res) {
   let end = begin + perPage;
   let products = db.get('products').value();
 
+  if (req.signedCookies.userId) {
+    let user = db.get('users').find({ id: req.signedCookies.userId }).value();
+    if (user) {
+      res.locals.user = user;
+    }
+  }
+
   res.render('products/products.pug', {
     products: products.slice(begin, end),
     lengthPage: Math.ceil(products.length / perPage),
