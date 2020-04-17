be1cea11c13e5058bbb731cd0404ab6081112810
Tuan Trung <tranvantuantrung@gmail.com>
2020-04-17 19:39:16 +0700

    method get post put patch delete for api

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -5,3 +5,57 @@ module.exports.index = async function (req, res) {
 
   res.json(products);
 };
+
+module.exports.postCreate = async function (req, res) {
+  let product = await Product.create(req.body);
+
+  res.json(product);
+};
+
+module.exports.viewProduct = async function (req, res) {
+  let id = req.params.id;
+
+  let product = await Product.findById(id);
+
+  res.json(product);
+};
+
+module.exports.putProduct = async function (req, res) {
+  let id = req.params.id;
+
+  let product = await Product.findByIdAndUpdate(req.body);
+
+  if (!product) {
+    await Product.create(req.body);
+  }
+
+  res.json(product);
+};
+
+module.exports.putProduct = async function (req, res) {
+  let id = req.params.id;
+
+  let product = await Product.findByIdAndUpdate(id, req.body);
+
+  if (!product) {
+    await Product.create(req.body);
+  }
+
+  res.json(product);
+};
+
+module.exports.patchProduct = async function (req, res) {
+  let id = req.params.id;
+
+  let product = await Product.findByIdAndUpdate(id, req.body);
+
+  res.json(product);
+};
+
+module.exports.deleteProduct = async function (req, res) {
+  let id = req.params.id;
+
+  let product = await Product.findByIdAndDelete(id);
+
+  res.json(product);
+};
--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -4,5 +4,10 @@ const router = express.Router();
 const controller = require('../controllers/product.controller');
 
 router.get('/', controller.index);
+router.get('/:id', controller.viewProduct);
+router.post('/', controller.postCreate);
+router.put('/:id', controller.putProduct);
+router.patch('/:id', controller.patchProduct);
+router.delete('/:id', controller.deleteProduct);
 
 module.exports = router;
